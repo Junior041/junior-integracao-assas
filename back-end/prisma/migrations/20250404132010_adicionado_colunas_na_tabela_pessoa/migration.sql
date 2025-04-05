810cd490a8b6b84895120b8f922a5b3273ed883f
ismael <ismaeljunior1508@gmail.com>
2025-04-05 17:02:02 -0300

    feat: adicionado um usuario padrao quando o prisma migrate é realizado

--- back-end/prisma/migrations/20250404132010_adicionado_colunas_na_tabela_pessoa/migration.sql
+++ back-end/prisma/migrations/20250404132010_adicionado_colunas_na_tabela_pessoa/migration.sql
@@ -18,3 +18,46 @@ CREATE UNIQUE INDEX `Pessoa_telefone_key` ON `Pessoa`(`telefone`);
 
 -- CreateIndex
 CREATE UNIQUE INDEX `Pessoa_email_key` ON `Pessoa`(`email`);
+
+-- salva o UUID da pessoa em uma variável
+SET @id_pessoa := UUID();
+
+-- insere a pessoa com esse UUID
+INSERT INTO Pessoa (
+  id_pessoa,
+  nome,
+  cpf_cnpj,
+  fk_user_create,
+  telefone,
+  email,
+  data_nascimento,
+  created_at
+) VALUES (
+  @id_pessoa,
+  'João da Silva',
+  '69764171095',
+  '11111111-2222-3333-4444-555555555555',
+  '48999990000',
+  'junior@teste.com',
+  '1990-05-15',
+  NOW()
+);
+
+-- usa o mesmo id da pessoa aqui
+INSERT INTO Usuario (
+  id_usuario,
+  fk_pessoa,
+  email,
+  senha,
+  ativo,
+  created_at,
+  updated_at
+) VALUES (
+  UUID(),
+  @id_pessoa,
+  'joao@email.com',
+  '$2b$08$fkUJ0BJEfn9coaRMO/9p3.UnuR.lvQVAFZ5QM3cToH55xcsGo/Fle', --imobia
+  true,
+  NOW(),
+  NOW()
+);
